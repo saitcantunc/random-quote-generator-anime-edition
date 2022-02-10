fbd027bcc5756212ea8e393b4b473d948b02c5fb
saitcantunc <tuncsaitcan@gmail.com>
2022-02-10 12:04:47 +0300

    fetch data and generate quote function done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+// Components
+import Quote from "./components/Quote";
+
 function App() {
-	return <div className='App'>app</div>;
+	const [quote, setQuote] = useState({
+		anime: null,
+		character: null,
+		quote: null,
+	});
+
+	const fetchQuote = async () => {
+		return await fetch("https://animechan.vercel.app/api/random").then((response) =>
+			response.json()
+		);
+	};
+
+	const generate = async () => {
+		setQuote(await fetchQuote());
+	};
+
+	useEffect(async () => {
+		setQuote(await fetchQuote());
+	}, []);
+
+	return (
+		<div className='App'>
+			<Quote quote={quote} />
+
+			<button onClick={generate}>Generate new quote</button>
+		</div>
+	);
 }
 
 export default App;
